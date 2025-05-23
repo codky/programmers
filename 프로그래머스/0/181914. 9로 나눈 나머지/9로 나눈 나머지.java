313e305ed3474c598c12f989c7b727c927dd947f
kyeon minseok <msky0912@gmail.com>
2025-05-23 16:09:36 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 63.79 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        int sum = 0;
+        String[] number_list = number.split("");
+        for(int i=0; i<number_list.length; i++) {
+            sum += Integer.parseInt(number_list[i]);
+        }
+        return sum % 9;
+    }
+}
