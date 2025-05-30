d3910946e435aef7221ad957b2a5375569d6ab2f
kyeon minseok <msky0912@gmail.com>
2025-05-30 22:30:35 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,18 +1,11 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int len = start_num - end_num + 1; //10-3=7 3까지 포함하므로 7+1=8
+        int len = start_num - end_num + 1;
         int[] answer = new int[len];
         
-        //0~7까지 8번
         for(int i=0; i<len; i++) {
-            //10-0=10
-            //10-1=9
-            //10-2=8
-            //...
-            //10-7=3
             answer[i] = start_num - i;
         }
-        
         return answer;
     }
 }
