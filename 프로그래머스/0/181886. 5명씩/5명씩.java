684ecb68cd04272b5059cf2910713396739780e6
kyeon minseok <msky0912@gmail.com>
2025-06-02 17:38:45 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String[] solution(String[] names) {
+        
+        int size = (names.length + 4) / 5;
+        
+        String[] answer = new String[size];
+        
+        for (int i = 0; i < size; i++) {
+            answer[i] = names[i * 5];
+        }
+
+        return answer;
+    }
+}
